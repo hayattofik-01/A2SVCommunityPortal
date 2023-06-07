1a38f4a6e2a91c0f0d8ed2605ad5aab62c494472
hayattofik-01 <hayattofik22@gmail.com>
2023-06-07 16:01:17 +0300

    add login entity

---
+++ a2sv_community_portal_mobile/lib/features/authentication/domain/entities/login.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+
+class Login extends Equatable {
+  final String token;
+
+  Login({required this.token}) : super();
+  
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
