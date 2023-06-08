287f0133dde1ad6680d7420fe94f6d04d9ae5f78
hayattofik-01 <hayattofik22@gmail.com>
2023-06-08 14:13:49 +0300

    add test for login model

---
+++ a2sv_community_portal_mobile/test/features/authentiation/data/model/login_model_test.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:a2sv_community_portal_mobile/features/authentication/data/model/login_model.dart';
+import 'package:a2sv_community_portal_mobile/features/authentication/domain/entities/login.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  final tLoginModel = LoginModel(
+     token: "1234");
+
+  test("Should be a subclass of Login Entity", () async {
+    expect(tLoginModel, isA<Login>());
+  });
+  test("Should return a valid model when the JSON is valid", () async {
+    final Map<String, dynamic> jsonMap = json.decode(fixture("user_login.json"));
+
+    final result = LoginModel.fromJson(jsonMap);
+    expect(result, tLoginModel);
+  });
+
+  test('Should return a JSON map with the proper data', () async {
+    final result = tLoginModel.toJson();
+    final expectedMap = {
+      "token": "1234"
+    };
+    expect(result, expectedMap);
+  });
+}
