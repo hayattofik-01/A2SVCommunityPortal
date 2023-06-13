db41541b1746e16602522b1dc2bccb56125fec5f
hayattofik-01 <hayattofik22@gmail.com>
2023-06-13 10:18:07 +0300

    fix server failures message

--- a2sv_community_portal_mobile/lib/core/utils/constants.dart
+++ a2sv_community_portal_mobile/lib/core/utils/constants.dart
@@ -2,6 +2,6 @@
 const String CACHED_TOKEN = 'CACHED_TOKEN';
 const String  CACHE_EXCEPTION= 'Login data not found';
 const String NO_CONNECTION_ERROR = 'Connection not Stable ';
-const String SERVER_FALIURE= 'Server faliure';
+const String SERVER_FALIURE= 'Something went wrong';
 const String LOGIN_FAILED = "Invalid username or passworld";
 const String UNKNOWN = "Unknown Error occured";
