6f5d6f3179513fc80993d165e6dffb7b1ec44946
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-06-14 11:49:43 +0300

    fix(add the removed pubspec.yaml file) (#9)
    * Create pubspec.yaml
    
    * add the core folder

---
+++ a2sv_community_portal_mobile/lib/core/utils/constants.dart
@@ -0,0 +1,7 @@
+
+const String cachedToken = 'CACHED_TOKEN';
+const String  cacheException= 'Login data not found';
+const String noConnectionError = 'Connection not Stable ';
+const String serverFaliure= 'Something went wrong';
+const String loginFailed = "Invalid username or passworld";
+const String unknown = "Unknown Error occured";
