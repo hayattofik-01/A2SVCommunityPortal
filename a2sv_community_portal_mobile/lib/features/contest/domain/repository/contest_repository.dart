53843475e6af325931503b75139311d14f8d929d
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-06-14 11:53:06 +0300

    write the domain layer of contest feature

---
+++ a2sv_community_portal_mobile/lib/features/contest/domain/repository/contest_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import '../entities/contest.dart';
+import '../../../../core/errors/failures.dart';
+
+abstract class ContestRepository {
+  Future<Either<Failure,List<Contest>>> getUpcomingContests();
+  Future<Either<Failure,List<Contest>>> getPastContests();
+}
