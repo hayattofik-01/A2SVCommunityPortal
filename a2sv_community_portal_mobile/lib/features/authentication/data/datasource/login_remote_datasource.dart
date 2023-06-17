24aad1e718e63881f574b17d390383251861f763
hayattofik-01 <hayattofik22@gmail.com>
2023-06-17 20:32:07 +0300

    fix data layer tests

---
+++ a2sv_community_portal_mobile/lib/features/authentication/data/datasource/login_remote_datasource.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:a2sv_community_portal_mobile/core/errors/exceptions.dart';
+import 'package:a2sv_community_portal_mobile/core/utils/constants.dart';
+import '../../domain/entities/login.dart';
+import '../model/login_model.dart';
+import 'package:http/http.dart' as http;
+
+abstract class LoginRemoteDataSource {
+  Future<Login> loginUser(String email, String password);
+}
+
+class LoginRemoteDataSourceImpl extends LoginRemoteDataSource {
+  
+final baseUrl = 'https://mocki.io/v1/a1c63369-aaee-437c-a86a-ee544f50268a';
+  final http.Client client;
+  LoginRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<LoginModel> loginUser(String email, String password) async {
+    final response = await client.post(Uri.parse(baseUrl),body:jsonEncode({
+      'email': email,
+      'password': password,
+    }));
+    if (response.statusCode == 401){
+      throw LoginFailedException(loginFailed);
+    }
+    if (response.statusCode != 200) {
+      throw ServerException(serverFaliure);
+    }
+    return LoginModel.fromJson(jsonDecode(response.body));
+  }
+}
---
+++ a2sv_community_portal_mobile/lib/features/authentication/data/repository/login_repo_impl.dart
@@ -0,0 +1,47 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/exceptions.dart';
+import '../../../../core/errors/failures.dart';
+import '../../../../core/network/network_info.dart';
+import '../../../../core/utils/constants.dart';
+import '../../domain/entities/login.dart';
+import '../../domain/repositories/login_repository.dart';
+import '../datasource/login_local_datasource.dart';
+import '../datasource/login_remote_datasource.dart';
+
+class LoginRepositoryImpl implements LoginRepository {
+  final LoginRemoteDataSource remoteDataSource;
+  final LoginLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  LoginRepositoryImpl(
+      {required this.remoteDataSource,
+      required this.localDataSource,
+      required this.networkInfo});
+
+  @override
+  Future<Either<Failure, Login>> loginUser(
+      String email, String password) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteData = await remoteDataSource.loginUser(email, password);
+        localDataSource.cacheToken(remoteData);
+        return Right(remoteData);
+      } on ServerException {
+        return const Left(ServerFailure(serverFaliure));
+      }
+    } else {
+      return const Left(NoConnectionFailure(noConnectionError));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Login>> fetchCachedToken() async {
+    try {
+      final localData = await localDataSource.getLastToken();
+      return Right(localData);
+    } on CacheException {
+      return const Left(CacheFailure(cacheException));
+    }
+  }
+}
+
