764dbb0454977be9f85f3550b9ac72b4779ed6a5
Simon Mekonnen <simonmekonnen80@gmail.com>
2023-06-22 20:47:37 -0800

    feat(mobile : simon) profile injection

--- a2sv_community_portal_mobile/lib/features/user_profile/domain/usecases/edit_user_profile.dart
+++ a2sv_community_portal_mobile/lib/features/user_profile/domain/usecases/edit_user_profile.dart
@@ -6,12 +6,12 @@ import '../entities/user_entity.dart';
 import '../repositories/user_repository.dart';
 
 class EditUserProfile implements UseCase<UserEntity, UserEntity> {
-  final UserRepository userRepository;
+  final UserRepository repository;
 
-  EditUserProfile(this.userRepository);
+  EditUserProfile({required this.repository});
 
   @override
   Future<Either<Failure, UserEntity>> call(UserEntity user) async {
-    return await userRepository.editUserProfile(user);
+    return await repository.editUserProfile(user);
   }
 }
