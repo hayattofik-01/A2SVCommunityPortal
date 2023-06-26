a9fe8310ed7e01dee27e115a83e8edf848ebdd71
Elisur Freyesus <98797447+Elizura@users.noreply.github.com>
2023-06-26 10:29:42 +0300

    Elizur.routing (#26)
    * [Mobile] added go router initial files
    
    * router error fix
    
    * more fixes

---
+++ a2sv_community_portal_mobile/lib/features/routes/routes.dart
@@ -0,0 +1,16 @@
+class Routes {
+  static const String HOME = '/';
+  static const String LOGIN = '/login';
+  static const String SIGNUP = '/signup';
+  static const String SPLASH = '/splash';
+  static const String PROFILE = '/profile';
+  static const String EDITPROFILE = '/editprofile';
+  static const String NOTIFICATION = '/notification';
+  static const String ANNOUNCEMENTS = '/announcements';
+  static const String ANNOUNCEMENTDETAILS = '/announcementdetails';
+  static const String CONTESTS = '/contests';
+  static const String CONTESTDETAILS = '/contestdetails';
+  static const String PROGRESS = '/progress';
+  static const String APPLICATION = '/application';
+  static const String APPLICATION1 = '/applicationdetails1';
+}
