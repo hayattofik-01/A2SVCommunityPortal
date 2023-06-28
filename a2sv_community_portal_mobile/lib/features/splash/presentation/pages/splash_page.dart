21374ccf9580ea1084943debd1b13f622c902c75
duressa <duressajemal3@gmail.com>
2023-06-28 11:27:30 +0300

    feat(mobile): Add routing for the whole app

--- a2sv_community_portal_mobile/lib/features/splash/presentation/pages/splash_page.dart
+++ a2sv_community_portal_mobile/lib/features/splash/presentation/pages/splash_page.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      GoRouter.of(context).go('/onboarding');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        backgroundColor: Color.fromRGBO(49, 130, 206, 1),
-        body: Center(
-          child: SvgPicture.asset(
-            'assets/svg/splash.svg',
-            width: 232,
-            height: 117,
-          ),
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(49, 130, 206, 1),
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/svg/splash.svg',
+          width: 232,
+          height: 117,
         ),
-      );
+      ),
+    );
   }
 }
