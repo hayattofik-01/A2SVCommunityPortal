16debfed7fba05814ee3f58ccdc0eb99cfb5172f
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-06-29 04:41:23 -0800

    fix(mobile : simon) token fix (#49)

--- a2sv_community_portal_mobile/lib/features/announcement_page/data/data_source/announcement_datasource.dart
+++ a2sv_community_portal_mobile/lib/features/announcement_page/data/data_source/announcement_datasource.dart
@@ -4,6 +4,7 @@ import 'package:a2sv_community_portal_mobile/core/errors/failures.dart';
 import 'package:a2sv_community_portal_mobile/features/announcement_page/data/model/announcement_model.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../core/errors/exceptions.dart';
 import '../../../../core/utils/constants.dart';
@@ -13,13 +14,14 @@ import 'package:http/http.dart' as http;
 class DataSource implements AnnouncementRemoteDataSource {
   final http.Client client;
   final url;
-  String token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlNmM1MmQ1Ny0yNGI2LTQ1MjQtYmUxMC1lYjdiY2U0ZDMyMTciLCJqdGkiOiIwM2YzNGU2NS1iZDA0LTQ0ZGMtYTM2MC03Mzc1MmM0MzA3MTEiLCJlbWFpbCI6ImFkbWluQGxvY2FsaG9zdC5jb20iLCJ1bmlxdWVfbmFtZSI6ImFkbWluQGxvY2FsaG9zdC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJIZWFkT2ZFZHVjYXRpb24iLCJleHAiOjE2OTA0MTU4NjYsImlzcyI6IkEyU1YgQ29tbXVuaXR5IFBvcnRhbCIsImF1ZCI6IkEyU1YgQ29tbXVuaXR5IFBvcnRhbCJ9.ki_D9QAQwHVS1e62TKvIuJNdFhb5jyF-cjiGN4t4hUY";
-  DataSource({required this.client})
+  final SharedPreferences sharedPreferences;
+  DataSource({required this.client, required this.sharedPreferences})
       : url = Uri.parse(
             "https://a2sv-community-portal-api.onrender.com/api/Blogs");
   @override
   Future<List<AnnouncementModel>> getAnnouncements() async {
+    String token =
+        jsonDecode(sharedPreferences.getString(cachedToken)!)['token'];
     final response = await client.get(url, headers: {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer $token'
