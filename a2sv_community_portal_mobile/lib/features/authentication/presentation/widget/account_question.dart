f9cd8f2697b5dfc98fc47c15aa2dd6453f6a615d
duressa <duressajemal3@gmail.com>
2023-06-30 10:28:57 +0300

    fix(mobile): Fix signup AccoungQuestion custom widget

--- a2sv_community_portal_mobile/lib/features/authentication/presentation/widget/account_question.dart
+++ a2sv_community_portal_mobile/lib/features/authentication/presentation/widget/account_question.dart
@@ -1,48 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../core/utils/colors.dart';
 import '../../../../core/utils/media_query.dart';
 
 class AccountQuestion extends StatelessWidget {
-final String question;
-final String action;
-final dynamic page;
-  const AccountQuestion({super.key, required this.question,required this.action,required this.page});
-  
+  final String question;
+  final String action;
+
+  const AccountQuestion({
+    super.key,
+    required this.question,
+    required this.action,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        margin: EdgeInsets.symmetric(vertical:UIConverter.getComponentWidth(context, 20)),
-       
-        child: Row(
-         
-          children: <Widget>[
-            Text(
-             question,
-              style: TextStyle(fontFamily: 'Poppins',fontSize: UIConverter.textSize(context, 16), fontWeight: FontWeight.w400,color: grey),
-            ),
-            SizedBox(
-              width:UIConverter.getComponentWidth(context, 10),
-            ),
-            InkWell(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) =>  page));
-      },
-            child: Text(
-            action,
-              style: TextStyle(
+    return Container(
+      margin: EdgeInsets.symmetric(
+          vertical: UIConverter.getComponentWidth(context, 20)),
+      child: Row(
+        children: <Widget>[
+          Text(
+            question,
+            style: TextStyle(
                 fontFamily: 'Poppins',
-                  color:blue,
+                fontSize: UIConverter.textSize(context, 16),
+                fontWeight: FontWeight.w400,
+                color: grey),
+          ),
+          SizedBox(
+            width: UIConverter.getComponentWidth(context, 10),
+          ),
+          InkWell(
+            onTap: () {
+              if (action == "SignUp") {
+                context.go("/signUp");
+              } else {
+                context.go("/login");
+              }
+            },
+            child: Text(
+              action,
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  color: blue,
                   fontSize: UIConverter.textSize(context, 14),
                   fontWeight: FontWeight.w600),
-            ),)
-          ],
-        ),
-      );
+            ),
+          )
+        ],
+      ),
+    );
   }
-
-    
-  }
-  
-  
+}
